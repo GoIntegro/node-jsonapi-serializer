ed2400c1d524e49bbb4deee650c0f508d75bc63f
nicosefer <nicolas.sefercheoglou@gmail.com>
2020-10-01 09:22:59 -0300

    adding more tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 import { JSONAPIDeserializer } from "./";
 
-test("basic", async () => {
+test("JSONAPIDeserializer:basic attributes", async () => {
   const jsonapiResponse = {
     data: {
       id: "1",
@@ -11,7 +11,32 @@ test("basic", async () => {
     },
   };
   const output = await JSONAPIDeserializer.deserialize(jsonapiResponse);
-  expect(output).toEqual({
-    data: { id: "1", type: "users", firstName: "fruta" },
-  });
+  expect(output).toHaveProperty("data");
+  const { data } = output;
+  expect(data).toHaveProperty("type");
+  expect(data).toHaveProperty("firstName");
+});
+
+test("JSONAPIDeserializer:basic relationships", async () => {
+  const jsonapiResponse = {
+    data: {
+      id: "1",
+      type: "users",
+      attributes: {
+        "first-name": "fruta",
+      },
+      relationships: {
+        photos: {
+          data: [
+            { id: "1", type: "files" },
+            { id: "2", type: "files" },
+          ],
+        },
+      },
+    },
+  };
+  const output = await JSONAPIDeserializer.deserialize(jsonapiResponse);
+  expect(output).toHaveProperty("data");
+  const { data } = output;
+  expect(data.photos).toEqual(["1", "2"]);
 });
